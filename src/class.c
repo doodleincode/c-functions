de8ce47f1cccde789a5ef2712687d847d7283a0e
Daniel Hong <daniel@int16.org>
2014-02-20 19:23:17 -0500

    Added class functions

---
+++ src/class.c
@@ -0,0 +1,19 @@
+/**
+ * Object related functions
+ *
+ * @author 	Daniel Hong
+ * 
+ * This program is licensed under the GNU GENERAL PUBLIC LICENSE Version 2.
+ * A LICENSE file should have accompanied this program.
+ */
+ 
+#include <stdlib.h>
+#include "../include/class.h"
+
+void release(void *ptr)
+{
+    if (ptr) {
+        free(ptr);
+        ptr = NULL;
+    }
+}
